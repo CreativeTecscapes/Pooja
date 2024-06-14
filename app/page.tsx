2af043c9bc3e81af184ef8899f5210b41397d51c
Rudalph <gonsalvesrudalph@gmail.com>
2024-06-14 12:03:14 +0530

    About section added

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,20 @@ import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import Typing from "@/components/Typing";
 import Svg from "@/components/Svg";
+import About from "@/components/About";
 export default function Home() {
   return (
     <>
       <Navbar />
       <div className="flex flex-col md:flex-row items-center">
-  <div className="md:mr-4">
-    <Typing />
-  </div>
-  <div className="md:fixed md:right-32 mt-8 md:mt-36">
-    <Svg />
-  </div>
-</div>
+        <div className="md:mr-4">
+          <Typing />
+        </div>
+        <div className="md:fixed md:right-32 mt-8 md:mt-36 lg:ml-0 ml-6">
+          <Svg />
+        </div>
+      </div>
+      <About />
     </>
   );
 }
---
+++ components/About.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+
+const words = `I am Pooja Bangar. I am a Graphic Gesigner by profession, I am a freelancer as well, I have completed my education in Graphic Designing and now moving for my higher studies in US
+I am passaniote about my work and my hobbies are singing dancing.
+`;
+
+export default function TextGenerateEffectDemo() {
+  return(
+    <div className="mt-32">
+        <div className="flex justify-center items-baseline align-middle mt-10 text-3xl text-[#3B82F6]"><strong>About Me</strong></div>
+        <div className="text-center text-sm lg:px-40 px-7 mt-10">
+           <TextGenerateEffect words={words}/>
+        </div>
+    </div>
+   
+  ) 
+}
